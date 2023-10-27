7eb927a833d6acbf56ab4b2bf8075195e1f0436d
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-27 22:00:09 +0200

    styles  link

--- src/components/header/nav/Link/index.jsx
+++ src/components/header/nav/Link/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./style.module.scss";
+import Link from "next/link";
 
-const Index = () => {
-  return <div>index</div>;
+const Index = ({ data }) => {
+  return (
+    <div className={styles.link}>
+      <Link>{data.title}</Link>
+    </div>
+  );
 };
 
 export default Index;
